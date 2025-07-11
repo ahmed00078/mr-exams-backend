68787e107532cd7d83b0cc6263579f83de96319b
ahmed00078 <ahmedsidimohammed78@gmail.com>
2025-07-11 09:52:57 +0200

    second commit

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ app.include_router(sessions.router)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        "app.main:app",
+        "main:app",
         host="0.0.0.0",
         port=8000,
         reload=settings.debug,
